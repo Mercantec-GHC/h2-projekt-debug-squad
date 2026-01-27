e13d629b39a070581e3e2a73132e4f26a8303c8d
Yana-pr25 <yanap2507@gmail.com>
2026-01-27 12:35:45 +0100

    dto

---
+++ Application/DTOs/RoomDto.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class RoomDto   // DTO for sending room info to client
+    {
+        public int Id { get; set; }
+        public string Number { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public decimal PricePerNight { get; set; }
+    }
+
+    public class CreateRoomDto // DTO for creating a new room from client
+    {
+        public string Number { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public decimal PricePerNight { get; set; }
+    }
+}
--- Domain/Entities/Room.cs
+++ Domain/Entities/Room.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace Domain.Entities
 {
-    internal class Room
+    public class Room
     {
+        public int Id { get; private set; }
+        public string Number { get; private set; } = string.Empty;
+        public int Capacity { get; private set; }
+        public decimal PricePerNight { get; private set; }
+
+
+        // Constructor
+        public Room(string number, int capacity, decimal pricePerNight)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+                throw new ArgumentException("Room number is required");
+
+            if (capacity <= 0)
+                throw new ArgumentException("Capacity must be greater than 0");
+
+            if (pricePerNight <= 0)
+                throw new ArgumentException("Price must be greater than 0");
+
+            Number = number;
+            Capacity = capacity;
+            PricePerNight = pricePerNight;
+        }
+
+        //  Update room details
+        public void UpdateDetails(string number, int capacity, decimal pricePerNight)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+                throw new ArgumentException("Room number is required");
+
+            if (capacity <= 0)
+                throw new ArgumentException("Capacity must be greater than 0");
+
+            if (pricePerNight <= 0)
+                throw new ArgumentException("Price must be greater than 0");
+
+            Number = number;
+            Capacity = capacity;
+            PricePerNight = pricePerNight;
+        }
     }
 }
+    
+
