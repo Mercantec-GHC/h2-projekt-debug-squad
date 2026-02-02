1441d194877e498817845b774ea6e2d18b9ab505
Kasper Lundby <venoelundby@gmail.com>
2026-02-02 11:28:01 +0100

    display rooms

---
+++ Client/Models/RoomsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Models
+{
+    public class RoomDto
+    {
+        public int RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public decimal PricePerNight { get; set; }
+    }
+}
