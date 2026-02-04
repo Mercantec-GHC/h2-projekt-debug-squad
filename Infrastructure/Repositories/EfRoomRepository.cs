cef2c34a9d3ff8a247ced8881242d4407552fc12
Kasper Lundby <venoelundby@gmail.com>
2026-02-04 10:36:39 +0100

    edit room ef not done

--- Infrastructure/Repositories/EfRoomRepository.cs
+++ Infrastructure/Repositories/EfRoomRepository.cs
@@ -40,17 +40,17 @@ namespace Infrastructure.Repositories
             await _dbContext.SaveChangesAsync();
         }
 
-        /*public async Task EditAsync(Room room)
-        {
-            var existingRoom = await _dbContext.Rooms.FindAsync(room.Id);
-            if (existingRoom == null)
-                throw new Exception("Room not found");
+        //public async Task EditAsync(Room room)
+        //{
+        //    var existingRoom = await _dbContext.Rooms.FindAsync(room.Id);
+        //    if (existingRoom == null)
+        //        throw new Exception("Room not found");
 
-            existingRoom.Name = room.Name;
-            existingRoom.Capacity = room.Capacity;
-            // update only what you allow
+        //    existingRoom.Number = room.Number;
+        //    existingRoom.Capacity = room.Capacity;
+        //    // update only what you allow
 
-            await _dbContext.SaveChangesAsync();
-        }*/
+        //    await _dbContext.SaveChangesAsync();
+        //}
     }
 }
