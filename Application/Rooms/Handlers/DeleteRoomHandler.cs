0b5b56ef6d18880184961c78123487282f6f3cd2
Andrii Bondar <bondar_andriy@ukr.net>
2026-02-24 13:59:32 +0100

    Commands

--- Application/Rooms/Handlers/DeleteRoomHandler.cs
+++ Application/Rooms/Handlers/DeleteRoomHandler.cs
@@ -5,19 +5,28 @@ namespace Application.Rooms.Handlers
 {
     public class DeleteRoomHandler
     {
-        private readonly IRoomRepository _repository;
+        private readonly IRoomRepository _roomRepository;
+        private readonly IBookingRepository _bookingRepository;
 
-        public DeleteRoomHandler(IRoomRepository repository)
+        public DeleteRoomHandler(IRoomRepository roomRepository, IBookingRepository bookingRepository)
         {
-            _repository = repository;
+            _roomRepository = roomRepository;
+            _bookingRepository = bookingRepository;
         }
 
         public async Task Handle(int id)
         {
-            Room? room = await _repository.GetByIdAsync(id) ?? throw new ArgumentException("Room not found.");
-            _repository.Delete(room);
+            Room? room = await _roomRepository.GetByIdAsync(id) ?? throw new ArgumentException("Room not found.");
 
-            await _repository.SaveChangesAsync();
+            var bookings = await _bookingRepository.GetAllAsync();
+            bool hasBookings = bookings.Any(b => b.Room.Id == id);
+
+            if (hasBookings)
+                throw new InvalidOperationException("Cannot delete a room that has bookings.");
+
+            _roomRepository.Delete(room);
+
+            await _roomRepository.SaveChangesAsync();
         }
     }
 }
